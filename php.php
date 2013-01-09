c7f7a578f63c0ef6ecd8242ad02337eb78c15732
naresh <naresh@purpletalk.com>
2013-01-09 10:25:59 +0530

    adding php.php file

---
+++ php.php
@@ -0,0 +1,15 @@
+<?php echo $_SERVER['HTTP_HOST']; ?>
+<br>
+<?php echo strtolower($_SERVER['HTTP_HOST']); ?>
+<br>
+<?php echo strtoupper($_SERVER['HTTP_HOST']); ?>
+<br>
+<?php echo str_replace('www.', '', $_SERVER['HTTP_HOST']); ?>
+<br>
+<?php echo str_replace('www.', '', strtolower($_SERVER['HTTP_HOST'])); ?>
+
+
+<? phpinfo(); ?>
+
+<?php phpinfo() ?>
+
